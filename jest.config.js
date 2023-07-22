1f8847783a43d4d4f4e4e04da51938a9e6843aad
Guilherme Felicio <guifelicio7@gmail.com>
2023-07-21 22:46:04 -0300

    add jest setup

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  roots: ["<rootDir>/src"],
+  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.ts$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "@/(.*)": "<rootDir>/src/$1",
+  },
+};
